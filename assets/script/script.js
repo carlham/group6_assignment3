1c81c65266e32d15323906c8459be31bdd70bf2f
anoshc <anoshc@stud.ntnu.no>
2022-04-30 00:00:20 +0200

    added intersection observer

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,38 +3,77 @@ const pageFive = document.querySelector(".page-five");
 const pageSix = document.querySelector(".page-six");
 
 const options = {
-  root: null,
-  threshold: 0,
-  rootMargin: "-150px",
+    root: null,
+    threshold: 0,
+    rootMargin: "-150px",
 };
 
-const pageFourObserver = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      pageFour.classList.add("page-four__animator");
-      console.log("page-four");
-    }
-  });
+const pageFourObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageFour.classList.add("page-four__animator");
+            console.log("page-four");
+        }
+    });
 }, options);
 
-const pageFiveObserver = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      pageFive.classList.add("page-five__animator");
-      console.log("page-five");
-    }
-  });
+const pageFiveObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageFive.classList.add("page-five__animator");
+            console.log("page-five");
+        }
+    });
 }, options);
 
-const pageSixObserver = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      pageSix.classList.add("page-six__animator");
-      console.log("page-six");
-    }
-  });
+const pageSixObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageSix.classList.add("page-six__animator");
+            console.log("page-six");
+        }
+    });
 }, options);
 
 pageFourObserver.observe(pageFour);
 pageFiveObserver.observe(pageFive);
 pageSixObserver.observe(pageSix);
+
+
+const pageSeven = document.querySelector(".page-seven");
+const pageEight = document.querySelector(".page-eight");
+const pageNine = document.querySelector(".page-nine");
+
+
+
+const pageSevenObserver = new IntersectionObserver(function(entriesOne, observerOne) {
+    entriesOne.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageSeven.classList.add("page-seven__animation");
+
+        }
+    });
+}, options);
+
+
+const pageEightObserver = new IntersectionObserver(function(entriesOne, observerOne) {
+    entriesOne.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageEight.classList.add("page-eight__animation");
+        }
+    });
+}, options);
+
+
+const pageNineObserver = new IntersectionObserver(function(entriesOne, observerOne) {
+    entriesOne.forEach((entry) => {
+        if (entry.isIntersecting) {
+            pageNine.classList.add("page-nine__animation");
+
+        }
+    });
+}, options);
+
+pageSevenObserver.observe(pageSeven);
+pageEightObserver.observe(pageEight);
+pageNineObserver.observe(pageNine);
